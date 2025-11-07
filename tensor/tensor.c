fd945d823a5a3952bde7c848a9f251af5b62e89a
Iason Krommydas <iason.krom@gmail.com>
2025-11-07 00:50:38 -0600

    defines at the top

--- tensor/tensor.c
+++ tensor/tensor.c
@@ -6,10 +6,11 @@ The goal is to have the most minimal implementation possible so that I can
 learn the C API for Python extensions.
 */
 #define PY_SSIZE_T_CLEAN
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+
 #include <Python.h>
 
-#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
-#include <stddef.h> /* for offsetof() */
+#include <stddef.h>/* for offsetof() */
 
 #include <numpy/arrayobject.h>
 
